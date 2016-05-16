53d05587126ede48c7dde95aa8c03bbfe0bde673
olivieeer <olicallens@gmail.com>
2016-05-16 21:09:04 +0200

    4nd

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatApp
+ */
+angular.module('chatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/budget.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatApp.controller:BudgetCtrl
+ * @description
+ * # BudgetCtrl
+ * Controller of the chatApp
+ */
+angular.module('chatApp')
+  .controller('BudgetCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
